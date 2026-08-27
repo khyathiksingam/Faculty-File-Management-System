99ff34272411ab84e5190e585c156d977f1e5a7d
khyathiksingam <khyathiksingam@gmail.com>
2026-08-28 00:56:58 +0530

    fix: import fs module in server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const fs = require('fs');
 require('dotenv').config();
 
 const { initDB } = require('./config/db');
